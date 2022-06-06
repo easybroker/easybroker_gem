c73cc3e0d9f3f7c888d1c12cc41466c81d6a96a6
Omar Juárez <omar@easybroker.com>
2022-06-06 15:00:21 -0500

    Fix broken pagination for query params.

--- lib/easy_broker/query.rb
+++ lib/easy_broker/query.rb
@@ -2,6 +2,7 @@
 
 class EasyBroker::Query
   attr_reader :api_client, :endpoint, :query_params
+  DEFAULT_PAGE = 1
 
   def initialize(api_client, endpoint, query_params)
     @api_client = api_client
@@ -9,9 +10,15 @@ class EasyBroker::Query
     @query_params = query_params
   end
 
-  def get(page = 1)
-    query_params[:page] = page
+  def get(page = nil)
+    query_params[:page] = page_param(page)
     response = api_client.get(endpoint, query: query_params)
     JSON.parse(response.body, object_class: OpenStruct)
+  end
+
+  private
+
+  def page_param(page)
+    page || query_params[:page] || DEFAULT_PAGE
   end
 end
--- test/query_test.rb
+++ test/query_test.rb
@@ -3,15 +3,30 @@ require "test_helper"
 class QueryTest < Minitest::Test
   def test_get
     client = mock
-    client.expects(:get).with('test', query: { a: 2, page: 1 }).returns(mock_response)
+    client.expects(:get).with('test', query: { a: 2, page: EasyBroker::Query::DEFAULT_PAGE }).returns(mock_response)
 
     query = EasyBroker::Query.new(client, 'test', { a: 2 })
     results = query.get
     assert_equal results.a, 1
     assert_equal results.b, 2
+  end
 
+  def test_get_with_page_param
+    client = mock
     client.expects(:get).with('test', query: { a: 2, page: 2 }).returns(mock_response)
-    query.get(2)
+    query = EasyBroker::Query.new(client, 'test', { a: 2 })
+    results = query.get(2)
+    assert_equal results.a, 1
+    assert_equal results.b, 2
+  end
+
+  def test_get_with_initial_query_params
+    client = mock
+    client.expects(:get).with('test', query: { a: 2, page: 3 }).returns(mock_response)
+    query = EasyBroker::Query.new(client, 'test', { a: 2, page: 3 })
+    results = query.get
+    assert_equal results.a, 1
+    assert_equal results.b, 2
   end
 
   def mock_response
